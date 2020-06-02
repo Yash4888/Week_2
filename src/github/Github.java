897c6f61ceede776120112092e59798504e10834
Yash4888 <kaur9287@sheridanc.on.ca>
2020-06-02 19:13:06 -0400

    This is my second commit in Software fundamentals.

--- src/github/Github.java
+++ src/github/Github.java
@@ -17,6 +17,7 @@ public class Github {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("learn github");
+        System.out.println("Second kutte bhkai");
             
     }
     
